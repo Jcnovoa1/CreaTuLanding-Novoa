39aaca5f3a5ec522e2233c39087e65c186641193
JC Novoa <jcruznovoa@gmail.com>
2026-01-22 09:45:50 -0300

    Primera preentrega: estructura inicial y estilos

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,11 @@
+// src/components/CartWidget/CartWidget.jsx
+const CartWidget = () => {
+  return (
+    <div className="cart-widget">
+      🛒
+      <span>0</span> {/* Hardcodeamos el número por ahora */}
+    </div>
+  );
+};
+
+export default CartWidget;
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+// src/components/ItemListContainer/ItemListContainer.jsx
+const ItemListContainer = ({ greeting }) => {
+  return (
+    <div className="item-list-container">
+      <h2>{greeting}</h2>
+    </div>
+  );
+};
+
+export default ItemListContainer;
